0c95fc5fab6bed5ec0000a2b1994fc096f469bc2
rogerrcarvajal <roger.carvajal@gmail.com>
2025-07-27 00:47:12 -0400

    dashboard - actualizacion

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../src/protector.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - SWIBO</title>
+    <title>SWIBO - Dashboard</title>
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
